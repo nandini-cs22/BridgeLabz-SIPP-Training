2560d9a0d0e43553c5a93684e01d05a994beeaf7
nandini mittal <nandini250903@gmail.com>
2025-06-13 09:30:51 +0530

    [Nandini Mittal].Add Problems for Java Fundamentals on Day2

---
+++ JavaFundamentals/code3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Code3
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter distance in km");
+		double km = sc.nextInt();
+		double result = km * 1.6;
+		System.out.println("Distance in kilometers is " +result);
+	}
+}
+
+		
+		
+		
