dff54d1b0fea1fec1682652c6efead7d4e2c7209
nandini-cs22 <122636297+nandini-cs22@users.noreply.github.com>
2025-06-19 10:09:09 +0530

    Update code8.java

--- JavaFundamentals/code8.java
+++ JavaFundamentals/code8.java
@@ -6,11 +6,9 @@ class Code8
 	int totalPens = 14;
         int totalStudents = 3;
 
-        // Calculate pens per student and remaining pens
         int pensPerStudent = totalPens / totalStudents;
         int remainingPens = totalPens % totalStudents;
-
-        // Output
+		
         System.out.println("The Pen Per Student is " + pensPerStudent + 
                            " and the remaining pen not distributed is " + remainingPens);
 	}
